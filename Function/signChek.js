29791d3b28aab6a8569aabff77c2317945ed405a
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-06-05 23:48:16 +0300

    Create signChek.js

---
+++ Function/signChek.js
@@ -0,0 +1,15 @@
+function signChek(num1, num2, num3) {
+
+let result = calculation(num1, num2, num3);
+  
+  if (result <0) {
+     console.log("Negative");
+  } else {
+     console.log("Positive");
+  }
+  
+ function calculation(a,b,c) {
+ return a * b * c;
+ } 
+
+}
