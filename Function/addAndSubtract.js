1d9274419279aa48d16c5557ef49ce677afd6b7d
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-06-06 00:22:52 +0300

    Create addAndSubtract.js

---
+++ Function/addAndSubtract.js
@@ -0,0 +1,14 @@
+function addAndSubtract(number1, number2, number3) {
+
+function sum(number1, number2) {
+    return number1 + number2;
+}
+let sumOfTwo = sum(number1, number2);
+ 
+function subtract(sumOfTwo, number3) {
+    return sumOfTwo - number3;
+}
+let result = subtract(sumOfTwo, number3);
+
+return result;
+}
