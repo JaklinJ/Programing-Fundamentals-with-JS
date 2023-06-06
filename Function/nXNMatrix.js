9df79fb1d162b76b1497d9c0400d374a7dcf2e19
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-06-06 15:45:43 +0300

    Create nXNMatrix.js

---
+++ Function/nXNMatrix.js
@@ -0,0 +1,11 @@
+function nXNMatrix(num) {
+
+    let res = '';
+for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= num; j++) {
+        res += num + " ";
+    }
+    res += '\n';
+}
+ console.log(res);
+}
