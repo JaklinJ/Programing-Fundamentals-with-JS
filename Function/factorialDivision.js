3e787fd905c05daabe8c520a99325ac264cdc780
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-06-06 17:00:49 +0300

    Create factorialDivision.js

---
+++ Function/factorialDivision.js
@@ -0,0 +1,25 @@
+function factorialDivision(numOne, numTwo) {
+
+function firstFactorial(numOne) {
+    let firstFactorial = 1;
+
+    for (let i = numOne; i >= 1; i--) {
+    firstFactorial *= i;
+}
+return firstFactorial;
+}
+ let firstResult = firstFactorial(numOne);
+ 
+ function secondFactorial(numTwo) {
+    let secondFactorial = 1;
+
+    for (let i = numTwo; i >= 1; i--) {
+    secondFactorial *= i;
+}
+return secondFactorial;
+ }
+ let secondResult = secondFactorial(numTwo)
+let result = firstResult / secondResult;
+
+console.log(result.toFixed(2));
+}
