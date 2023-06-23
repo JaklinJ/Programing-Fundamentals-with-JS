ceaa7d3bde7008e3fb29b5aa061865ae555089bb
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-06-23 13:22:49 +0300

    Create personInfo.js

---
+++ Objects and Classes/personInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age) {
+    
+    let obj = {
+        firstName :  firstName,
+        lastName : lastName,
+        age : age
+    }
+    return obj;
+
+}
