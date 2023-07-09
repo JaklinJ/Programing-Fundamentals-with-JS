bb559298687179831cba345e6ecd8511c3b65e7a
Jaklin Jeleva <122488493+JaklinJ@users.noreply.github.com>
2023-07-09 22:43:02 +0300

    Update aMinerTask.js

--- Objects and Classes/aMinerTask.js
+++ Objects and Classes/aMinerTask.js
@@ -1,15 +1,19 @@
 function aMinerTask(data) {
 
-let mineStorage = {};
+    let mineStorage = {};
 
-for(let i = 0; i < data.length; i+=2) {
-    let resource = data[i];
-    let quantity = data[i+1];
+    for (let i = 0; i < data.length; i += 2) {
+        let resource = data[i];
+        let quantity = Number(data[i + 1]);
 
-    mineStorage[resource] = quantity;
-}
+        if (mineStorage.hasOwnProperty(resource)) {
+            mineStorage[resource] += quantity;
+        } else {
+            mineStorage[resource] = quantity;
+        }
+    }
 
-for (const key in mineStorage) {
-    console.log(`${key} -> ${mineStorage[key]}`)
+    for (const key in mineStorage) {
+        console.log(`${key} -> ${mineStorage[key]}`)
     }
 }
